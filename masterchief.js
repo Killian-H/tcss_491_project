7fce1cfc38eb3df4abcde1acc566d6933c3c8bab
Troy Zon <troyzon@gmail.com>
2022-01-17 12:50:30 -0800

    Local changes.

--- masterchief.js
+++ masterchief.js
@@ -25,8 +25,6 @@ class masterchief {
         this.state = 0; // 0 = idle, 1 = walking, 2 = idle crouch, 3 = crouch walking, 4 = melee, 5 = dead
         this.dead = false; // not dead initially
 
-        this.left = new Animator(this.IDLE_LEFT, 0, 0, 26, 43, 1, 1, false, true);
-        this.right = new Animator(this.IDLE_RIGHT, 0, 0, 26, 43, 1, 1, false, true);
         this.armRotation = 0;
         this.orientation = "right";
         this.x = this.X_DEFAULT;
@@ -35,7 +33,9 @@ class masterchief {
         //this.animator = new Animator(ASSET_MANAGER.getAsset("./sprites/master_chief/arms_1.png"), 3, 0, 38, 70, 1, 0.2);
         
         this.animations = [];
-        //this.loadAnimations();
+        this.loadAnimations();
+        this.leftAnim = this.animations[0][1];
+        this.rightAnim = this.animations[0][0];
 
     };
 
@@ -49,53 +49,53 @@ class masterchief {
 
         // idle animation for standing = 0
         // facing right = 0
-        this.animations.push[0][0] = new Animator(this.IDLE_RIGHT, 0, 0, 26, 43, 1, 1, false, true);
+        this.animations[0][0] = new Animator(this.IDLE_RIGHT, 0, 0, 26, 43, 1, 1, false, true);
 
         // idle animation for standing = 0
         // facing left = 1
-        this.animations.push[0][1] = new Animator(this.IDLE_LEFT, 1, 0, 26, 43, 1, 1, false, true);
+        this.animations[0][1] = new Animator(this.IDLE_LEFT, 1, 0, 26, 43, 1, 1, false, true);
 
         // walking animation = 1
         // facing right = 0
-        this.animations.push[1][0] = new Animator(this.WALK_RIGHT, 0, 2, 41, 41, 8, 0.1, false, true);
+        this.animations[1][0] = new Animator(this.WALK_RIGHT, 0, 2, 41, 41, 8, 0.1, false, true);
 
         // walking animation = 1
         // facing left = 1
-        this.animations.push[1][1] = new Animator(this.WALK_LEFT, 0, 2, 42, 42, 8, 0.1, true, true);
+        this.animations[1][1] = new Animator(this.WALK_LEFT, 0, 2, 42, 42, 8, 0.1, true, true);
 
         // idle animation for crouching = 2
         // facing right = 0
-        this.animations.push[2][0] = new Animator(this.IDLE_CROUCH_RIGHT, 1, 0, 37, 31, 1, 1, false, true);
+        this.animations[2][0] = new Animator(this.IDLE_CROUCH_RIGHT, 1, 0, 37, 31, 1, 1, false, true);
 
         // idle animation for crouching = 2
         // facing left = 1
-        this.animations.push[2][1] = new Animator(this.IDLE_CROUCH_LEFT, 0, 1, 37, 31, 1, 1, false, true);
+        this.animations[2][1] = new Animator(this.IDLE_CROUCH_LEFT, 0, 1, 37, 31, 1, 1, false, true);
 
         // crouch walking animation = 3
         // facing right = 0
         // Potentially cut last frame
-        this.animations.push[3][0] = new Animator(this.CROUCH_WALK_RIGHT, 0, 0, 36, 31, 4, 0.2, false, true);
+        this.animations[3][0] = new Animator(this.CROUCH_WALK_RIGHT, 0, 0, 36, 31, 4, 0.2, false, true);
 
         // crouch walking animation = 3
         // facing left = 1
         // Potentially cut last frame
-        this.animations.push[3][1] = new Animator(this.CROUCH_WALK_LEFT, 7, 4, 36, 31, 4, 0.2, true, true);
+        this.animations[3][1] = new Animator(this.CROUCH_WALK_LEFT, 7, 4, 36, 31, 4, 0.2, true, true);
 
         // Melee Right = 4
         // facing right = 0
-        this.animations.push[4][0] = new Animator(this.MELEE_RIGHT, 15, 1, 42, 44, 7, 0.1, false, false);
+        this.animations[4][0] = new Animator(this.MELEE_RIGHT, 15, 1, 42, 44, 7, 0.1, false, false);
 
         // Melee Right = 4
         // facing right = 1
-        this.animations.push[4][1] = new Animator(this.MELEE_LEFT, 15, 3, 40, 43, 7, 0.1, false, false);
+        this.animations[4][1] = new Animator(this.MELEE_LEFT, 15, 3, 40, 43, 7, 0.1, false, false);
 
         // dead right = 5
         // facing right = 0
-        this.animations.push[5][0] = new Animator(this.DEAD_RIGHT, 0, 1, 47, 43, 4, 0.14, false, false);
+        this.animations[5][0] = new Animator(this.DEAD_RIGHT, 0, 1, 47, 43, 4, 0.14, false, false);
 
         // dead right = 5
         // facing right = 1
-        this.animations.push[5][1] = new Animator(this.DEAD_LEFT, 30, 1, 50, 43, 4, 0.14, true, false);
+        this.animations[5][1] = new Animator(this.DEAD_LEFT, 30, 1, 50, 43, 4, 0.14, true, false);
     };
 
     update() {
@@ -106,13 +106,13 @@ class masterchief {
                 - (this.game.mouse.y - this.y)
             ) - 1.5708;
 
-            console.log(this.armRotation);
+            //console.log(this.armRotation);
             if(this.armRotation > -1.5037 && this.armRotation < 1.4825) {
                 this.orientation = "right";
-                console.log("Orientation right");
+                //console.log("Orientation right");
             } else {
                 this.orientation = "left";
-                console.log("Orientation left");
+                //console.log("Orientation left");
             }
         }
     };
@@ -121,9 +121,9 @@ class masterchief {
 
         ctx.save();
         if(this.orientation == "left") {
-            this.left.drawFrame(this.game.clockTick, ctx, this.X_DEFAULT- 2.5* 7.5, this.Y_DEFAULT - 7.5, this.SCALE);
+            this.leftAnim.drawFrame(this.game.clockTick, ctx, this.x- 2.5* 7.5, this.y - 7.5, this.SCALE);
         } else {
-            this.right.drawFrame(this.game.clockTick, ctx, this.X_DEFAULT - 7.5, this.Y_DEFAULT - 7.5, this.SCALE);
+            this.rightAnim.drawFrame(this.game.clockTick, ctx, this.x - 7.5, this.y - 7.5, this.SCALE);
         }
         ctx.restore();
 
