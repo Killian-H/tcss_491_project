14c20c9b6aa553ad245507570e4a70f415328c32
Killian-H <k-hickey1@hotmail.com>
2022-01-18 19:53:39 -0800

    Collision Detection For Chief Alpha

---
+++ boundingbox.js
@@ -0,0 +1,18 @@
+class BoundingBox {
+    constructor(x, y, width, height) {
+        Object.assign(this, {x, y, width, height});
+
+        this.left = x;
+        this.top = y;
+        this.right = this.left + this.width;
+        this.bottom = this.top + this.height;
+    };
+
+    collide(theOther) {
+        let isCollide = false;
+        if (this.right > theOther.left && this.left < theOther.right && this.top < theOther.bottom && this.bottom > theOther.top) {
+            isCollide = true;
+        }
+        return isCollide;
+    };
+};
