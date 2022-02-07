404c54504076d7a83d95a0d0dea95265990dc247
Levi McCoy (lmc56) <lmc56@uw.edu>
2022-02-06 19:17:20 -0800

    basic bars

--- hud.js
+++ hud.js
@@ -21,8 +21,25 @@ class hud {
         //ctx.font = PARAMS.BLOCKWIDTH/2 + 'px "Arial"';
         ctx.font = 'bold 48px serif';
         ctx.fillText((this.masterchief.ammo+"/"+this.masterchief.AMMO_DEFAULT), 25, 50);
-        ctx.fillText(("🛡️ "+this.masterchief.armor), 850, 50);
-        ctx.fillText(("➕ "+this.masterchief.health), 850, 110);
+        //ctx.fillText(("🛡️ "+this.masterchief.armor), 850, 50);
+        //ctx.fillText(("➕ "+this.masterchief.health), 850, 110);
+        var ratio = this.masterchief.health / this.masterchief.MAX_HEALTH;
+        ctx.font = 'bold 24px serif';
+        ctx.fillText(("➕"), 815, 105);
+        ctx.fillStyle = "Red";
+        ctx.fillRect(850, 75, 150*ratio, 40);
+        var ratio2 = this.masterchief.armor / this.masterchief.MAX_ARMOR;
+        ctx.fillText(("🛡️"), 820, 55);
+        ctx.fillStyle = "Blue";
+        ctx.fillRect(850, 25, 150*ratio2, 40);
+        ctx.font = 'bold 48px serif';
+        if (PARAMS.DEBUG == true) {
+            ctx.fillStyle = "Black";
+            ctx.strokeStyle = "Black";
+            ctx.font = 'bold 48px serif';
+            ctx.fillText(("🛡️ "+this.masterchief.armor), 850, 50);
+            ctx.fillText(("➕ "+this.masterchief.health), 850, 110);
+        }
         //ctx.strokeText((this.masterchief.ammo+"ammo"), 1000, 500);
 
 
