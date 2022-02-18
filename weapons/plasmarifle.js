950b17ca1755be16ce55762495ddfb33a8f1abb2
Killian-H <k-hickey1@hotmail.com>
2022-02-18 09:53:05 -0800

    addEntityToFront to addEntity

--- weapons/plasmarifle.js
+++ weapons/plasmarifle.js
@@ -32,35 +32,35 @@ class PlasmaRifle extends AbstractWeapon {
 
     update() {
         if(!this.game.pauseb){
-        this.x = this.game.player.x;
-        this.y = this.game.player.y;
-        const TICK = this.game.clockTick;
-        if(this.game.mouse != null) {
-            this.armRotation = Math.atan2 (
-                this.game.mouse.x - this.x, 
-                - (this.game.mouse.y - this.y)
-            ) - Math.PI / 2;
-        }
+            this.x = this.game.player.x;
+            this.y = this.game.player.y;
+            const TICK = this.game.clockTick;
+            if(this.game.mouse != null) {
+                this.armRotation = Math.atan2 (
+                    this.game.mouse.x - this.x, 
+                    - (this.game.mouse.y - this.y)
+                ) - Math.PI / 2;
+            }
 
-        this.elapsedtime += TICK;
-        //console.log(this.elapsedtime);
-        if(this.game.click != null && this.elapsedtime > this.firerate && this.ammo > 0 && !this.game.reload && this.canshoot) {
-            this.elapsedtime = 0;
-            this.clickcount = 1;
-            this.ammo -= 1;
+            this.elapsedtime += TICK;
+            //console.log(this.elapsedtime);
+            if(this.game.click != null && this.elapsedtime > this.firerate && this.ammo > 0 && !this.game.reload && this.canshoot) {
+                this.elapsedtime = 0;
+                this.clickcount = 1;
+                this.ammo -= 1;
 
-            this.game.addEntityToFront(new bullet(this.game, this.x, this.y, this.game.mouse.x + (getRandomRange(-.1, .1) * getDistance(this.x, this.y, this.game.mouse.x, this.game.mouse.y)), this.game.mouse.y + (getRandomRange(-.1, .1) * getDistance(this.x, this.y, this.game.mouse.x, this.game.mouse.y)), this.armRotation, this.speed, this.PR_BULLET, this.shieldDamage, this.healthDamage));
-            ASSET_MANAGER.playAsset("./audio/weapons/pr single shot.mp3");
-            //this.game.click = null
-        }
+                this.game.addEntityToFront(new bullet(this.game, this.x, this.y, this.game.mouse.x + (getRandomRange(-.1, .1) * getDistance(this.x, this.y, this.game.mouse.x, this.game.mouse.y)), this.game.mouse.y + (getRandomRange(-.1, .1) * getDistance(this.x, this.y, this.game.mouse.x, this.game.mouse.y)), this.armRotation, this.speed, this.PR_BULLET, this.shieldDamage, this.healthDamage));
+                ASSET_MANAGER.playAsset("./audio/weapons/pr single shot.mp3");
+                //this.game.click = null
+            }
 
-        if (this.game.reload && (this.ammo < this.AMMO_DEFAULT)&&!this.reloading) {
-            let stopShoot = setInterval(() => {this.canshoot = false,this.reloadTime += 1,this.reloading = true}, 1);
-            ASSET_MANAGER.playAsset("./audio/weapons/pr reload.mp3")
-            setTimeout(() => {this.ammo = this.AMMO_DEFAULT, clearInterval(stopShoot), this.canshoot = true,this.reloading = false,this.reloadTime = 0}, this.defaultReloadTime);
-            //clearInterval(() => {clearInterval(stopShoot), this.canshoot = true}, 3000);
+            if (this.game.reload && (this.ammo < this.AMMO_DEFAULT)&&!this.reloading) {
+                let stopShoot = setInterval(() => {this.canshoot = false,this.reloadTime += 1,this.reloading = true}, 1);
+                ASSET_MANAGER.playAsset("./audio/weapons/pr reload.mp3")
+                setTimeout(() => {this.ammo = this.AMMO_DEFAULT, clearInterval(stopShoot), this.canshoot = true,this.reloading = false,this.reloadTime = 0}, this.defaultReloadTime);
+                //clearInterval(() => {clearInterval(stopShoot), this.canshoot = true}, 3000);
+            }
         }
-    }
     };
 
     getArmRotation() {
